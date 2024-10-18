4734bc83b12684db24ac7b3be845a86a11cd3e9d
Oswald-Osei-Owusu-Asiedu <oswaldosei9@gmail.com>
2024-10-18 18:26:24 +0000

    documentation added

--- product/src/main/java/microservice/product/services/ProductServiceGRPC.java
+++ product/src/main/java/microservice/product/services/ProductServiceGRPC.java
@@ -1,5 +1,6 @@
 package microservice.product.services;
 
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import microservice.product.ProductRepository.ProductRepository;
 import microservice.product.entities.ProductEntity;
@@ -9,19 +10,21 @@ import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.stereotype.Service;
 
 @GrpcService
-public class ProductServiceGRPC extends ProductServiceGrpc.ProductServiceImplBase{
+public class ProductServiceGRPC extends ProductServiceGrpc.ProductServiceImplBase {
     private final ProductRepository productRepository;
 
-    public ProductServiceGRPC(ProductRepository productRepository){
+    public ProductServiceGRPC(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     @Override
     public void getProductById(ProductProto.ProductRequest request, StreamObserver<ProductProto.ProductResponse> responseObserver) {
-        long productId = request.getId();
+        try {
+            long productId = request.getId();
 
-        ProductEntity productEntity = productRepository.findById(productId).orElseThrow(null);
-        if(productEntity!=null){
+            ProductEntity productEntity = productRepository.findById(productId)
+                    .orElseThrow(() -> new RuntimeException("Product with ID " + productId + " doesn't exist"));
+
             ProductProto.Product product = ProductProto.Product.newBuilder()
                     .setId(productEntity.getId())
                     .setProductName(productEntity.getProductName())
@@ -29,15 +32,17 @@ public class ProductServiceGRPC extends ProductServiceGrpc.ProductServiceImplBas
                     .setProductPrice(productEntity.getProductPrice())
                     .setProductQuantity(productEntity.getProductQuantity())
                     .build();
+
             ProductProto.ProductResponse response = ProductProto.ProductResponse.newBuilder()
                     .setProduct(product)
                     .build();
-        }else{
-            responseObserver.onError(new RuntimeException("Product with ID"+ productId + "doesn't exist"));
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            responseObserver.onError(Status.INTERNAL
+                    .withDescription("Error retrieving product: " + e.getMessage())
+                    .asRuntimeException());
         }
-
-        //Complete the RPC call
-        responseObserver.onCompleted();
-
     }
 }
